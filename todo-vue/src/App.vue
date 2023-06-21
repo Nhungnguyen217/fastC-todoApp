94144fcbcf00e9eba1aaa6597f18a1e96aa78e14
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-06-21 10:48:11 +0700

    change

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,30 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+import { ref, onMounted, computed, watch } from "vue"
+const todos = ref([]) //create empty arr
+const name = ref("") //create empty string
+const input_content = ref("")
+const input_category = ref(null)
 
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return a.createdAt - b.createdAt
+}))
+
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal)
+})
+
+
+</script>
+  
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">Hi, this is to-do list of <input type="text" placeholder="name here" v-model="name" /></h2>
+    </section>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
