52d6a9fd1f915f502f960dfa21603c72f2669f8d
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-06-22 02:25:58 +0700

    change

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { ref, onMounted, computed, watch } from "vue"
+
+// tạo các biến, mảng dữ liệu
 const todos = ref([]) //create empty arr
 const name = ref("") //create empty string
 const input_content = ref("")
 const input_category = ref(null)
 
+// các hàm thực thi
 const todos_asc = computed(() => todos.value.sort((a, b) => {
-  return a.createdAt - b.createdAt
+  return b.createdAt - a.createdAt
 }))
 
+//thêm todo vào danh sạc to-do list
 const addTodo = () => {
   if (input_content.value.trim() === "" | input_category.value === null) {
     return
@@ -20,9 +24,16 @@ const addTodo = () => {
     createdAt: new Date().getTime(),
 
   })
+
+  console.log(input_content.value)
+  input_content.value = ''
+  input_category.value = null
 }
 
-// localStorage
+// hàm xóa todo
+const removeTodo = todo => { todos.value = todos.value.filter(t => t !== todo) }
+
+//lưu vào localStorage
 watch(todos, newVal => {
   localStorage.setItem("todos", JSON.stringify(newVal))
 }, { deep: true })
@@ -33,9 +44,11 @@ watch(name, newVal => {
 
 onMounted(() => {
   name.value = localStorage.getItem("name") || ""
+  todos.value = JSON.parse(localStorage.getItem("todos")) || []
 })
 </script>
   
+<!-- Sắp xếp -->
 <template>
   <main class="app">
     <section class="greeting">
@@ -47,23 +60,47 @@ onMounted(() => {
         <h4>What's on your to-do list?</h4>
         <input type="text" placeholder="e.g Make a to-do app by Vue3 " v-model="input_content" />
 
-        <!-- Set timeEnd of duration-->
-        <h4>Set duration:</h4>
+        <!-- TinyMCE format -->
+        <textarea placeholder="e.g Make a to-do app by Vue3" v-model="input_content"></textarea>
 
+
+        <!-- Thiết lập thời hạn cho to-do -->
+        <h4 id="duration">Set duration:</h4>
+
+        <!-- Phân loại to-do -->
         <h4>Pick a category</h4>
-        <div class="options">
-          <label> <input type="radio" name="category" id="category1" value="Work" v-model="input_category" /><span
+        <div class=" options">
+          <label> <input type="radio" name="category" id="category1" value="business" v-model="input_category" /><span
               class="bubble business"></span>
             <div>Work</div>
           </label>
-
-          <label> <input type="radio" name="category" id="category2" value="Personal" v-model="input_category" /><span
+          <label> <input type="radio" name="category" id="category2" value="personal" v-model="input_category" /><span
               class="bubble personal"></span>
             <div>Personal</div>
           </label>
         </div>
         <input type="submit" value="Add todo" />
       </form>
+    </section>
+
+    <!-- Hiển thị danh sách to-do -->
+    <section class="todo-list">
+      <h3>TO-DO LIST</h3>
+      <div class="list">
+        <!-- lấy từ todos_asc -->
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done">
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+          <div class="action">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
     </section>
   </main>
 </template>
