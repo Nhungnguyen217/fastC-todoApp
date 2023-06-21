531ca00dee52a36655a2a0b80fcd1e80026651fc
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-06-21 11:28:23 +0700

    change

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -9,17 +9,61 @@ const todos_asc = computed(() => todos.value.sort((a, b) => {
   return a.createdAt - b.createdAt
 }))
 
-watch(name, (newVal) => {
+const addTodo = () => {
+  if (input_content.value.trim() === "" | input_category.value === null) {
+    return
+  }
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime(),
+
+  })
+}
+
+// localStorage
+watch(todos, newVal => {
+  localStorage.setItem("todos", JSON.stringify(newVal))
+}, { deep: true })
+
+watch(name, newVal => {
   localStorage.setItem("name", newVal)
 })
 
-
+onMounted(() => {
+  name.value = localStorage.getItem("name") || ""
+})
 </script>
   
 <template>
   <main class="app">
     <section class="greeting">
       <h2 class="title">Hi, this is to-do list of <input type="text" placeholder="name here" v-model="name" /></h2>
+    </section>
+    <section class="create-todo">
+      <h3>CREATE A TO-DO</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your to-do list?</h4>
+        <input type="text" placeholder="e.g Make a to-do app by Vue3 " v-model="input_content" />
+
+        <!-- Set timeEnd of duration-->
+        <h4>Set duration:</h4>
+
+        <h4>Pick a category</h4>
+        <div class="options">
+          <label> <input type="radio" name="category" id="category1" value="Work" v-model="input_category" /><span
+              class="bubble business"></span>
+            <div>Work</div>
+          </label>
+
+          <label> <input type="radio" name="category" id="category2" value="Personal" v-model="input_category" /><span
+              class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+        <input type="submit" value="Add todo" />
+      </form>
     </section>
   </main>
 </template>
